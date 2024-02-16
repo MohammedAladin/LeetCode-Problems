7fbe9249dfbe2cd7cf1ac53ae947240838365456
MohammedAladin <69739865+MohammedAladin@users.noreply.github.com>
2024-02-16 15:10:53 +0200

    Time: 181 ms (41.95%) | Memory: 67.5 MB (41.25%) - LeetSync

--- daily_practice/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ daily_practice/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -5,14 +5,14 @@ class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
         map<int, int> frq;
-        int ans=0;
+        int ans=0, end = -1, cnt = 0;
         for(int i = 0; i<arr.size(); ++i){
             frq[arr[i]]++;
         }
+        
         vector<pair<int, int>> vec(frq.begin(), frq.end());
         sort(vec.begin(), vec.end(), perceed);
-        int end = -1;
-        int cnt = 0;
+    
         for(int i = 0; i<vec.size(); ++i){
             if(cnt + vec[i].second <=k)
                 cnt += vec[i].second;
