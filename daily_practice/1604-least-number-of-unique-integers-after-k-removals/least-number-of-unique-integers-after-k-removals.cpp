54f494879b602af053f8968fad212e0ca44231dc
MohammedAladin <69739865+MohammedAladin@users.noreply.github.com>
2024-02-16 15:14:52 +0200

    Time: 126 ms (70.92%) | Memory: 66.1 MB (51.33%) - LeetSync

--- daily_practice/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ daily_practice/1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -4,18 +4,21 @@ bool perceed(const pair<int, int> & a, const pair<int, int> & b){
 class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        map<int, int> frq;
+        unordered_map<int, int> frq;
+        vector<int> vec;
         int ans=0, end = -1, cnt = 0;
-        for(int i = 0; i<arr.size(); ++i){
-            frq[arr[i]]++;
+
+        for(auto a: arr){
+            frq[a]++;
         }
-        
-        vector<pair<int, int>> vec(frq.begin(), frq.end());
-        sort(vec.begin(), vec.end(), perceed);
+        for(auto a: frq){
+            vec.push_back(a.second);
+        }
+        sort(vec.begin(), vec.end());
     
         for(int i = 0; i<vec.size(); ++i){
-            if(cnt + vec[i].second <=k)
-                cnt += vec[i].second;
+            if(cnt + vec[i] <=k)
+                cnt += vec[i];
             else {
                 end = i;
                 break;
