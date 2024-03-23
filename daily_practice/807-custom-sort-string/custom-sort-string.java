df6d4b04002a1126803ffeb7c13159b81abbabc5
MohammedAladin <69739865+MohammedAladin@users.noreply.github.com>
2024-03-23 07:33:13 +0200

    Time: 5 ms (23.41%) | Memory: 41.1 MB (87.35%) - LeetSync

--- daily_practice/807-custom-sort-string/custom-sort-string.java
+++ daily_practice/807-custom-sort-string/custom-sort-string.java
@@ -1,17 +1,18 @@
 class Solution {
     public String customSortString(String order, String s) {
         StringBuilder ans = new StringBuilder();
-        
+        StringBuilder temp = new StringBuilder(s); // Create a mutable copy of s
+
         for(int i =0; i<order.length(); ++i){
             for(int j = 0; j<s.length(); ++j){
-                if(order.charAt(i) == s.charAt(j)){
+                if(order.charAt(i) == temp.charAt(j)){
                     ans.append(s.charAt(j));
-                    s = s.substring(0, j) + "#" + s.substring(j + 1);
+                    temp.setCharAt(j, '#'); // Modify the mutable copy
                 }
             }
         }
         for(int i = 0; i<s.length(); ++i){
-            if(s.charAt(i)!='#')
+            if(temp.charAt(i)!='#')
                 ans.append(s.charAt(i));
 
         }
