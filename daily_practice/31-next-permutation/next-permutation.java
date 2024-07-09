553118303f2c1a867158a1e0355150438dd3ebba
MohammedAladin <69739865+MohammedAladin@users.noreply.github.com>
2024-07-09 16:04:29 +0300

    Time: 0 ms (100.00%) | Memory: 42.8 MB (79.55%) - LeetSync

--- daily_practice/31-next-permutation/next-permutation.java
+++ daily_practice/31-next-permutation/next-permutation.java
@@ -1,41 +1,40 @@
- class Solution {
+class Solution {
 
-     public void nextPermutation(int[] nums) {
-         int lastPos = -1;
-         for(int i = nums.length-2; i>=0; --i){
-             if(nums[i] < nums[i+1]){
-                 lastPos = i;
-                 break;
-             }
-         }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 
-         if(lastPos==-1){
-             for(int i = 0; i<nums.length/2; ++i){
-                 int temp = nums[i];
-                 nums[i] = nums[nums.length-1-i];
-                 nums[nums.length-1-i] = temp;
-             }
-             return;
-         }
+    public void nextPermutation(int[] nums) {
+        int lastPos = -1;
+        for (int i = nums.length - 2; i >= 0; --i) {
+            if (nums[i] < nums[i + 1]) {
+                lastPos = i;
+                break;
+            }
+        }
 
-         int i = lastPos+1;
-         int j = nums.length-1;
-         while(j>i){
-             int temp = nums[i];
-             nums[i] = nums[j];
-             nums[j] = temp;
+        if (lastPos == -1) {
+            for (int i = 0; i < nums.length / 2; ++i) {
+                swap(nums, i, nums.length - 1 - i);
+            }
+            return;
+        }
 
-             i++;
-             j--;
-         }
-         for(int k = lastPos+1; k<nums.length; ++k){
-             if(nums[k]>nums[lastPos]){
-                 int temp = nums[k];
-                 nums[k] = nums[lastPos];
-                 nums[lastPos] = temp;
-                 break;
-             }
-         }
+        int i = lastPos + 1;
+        int j = nums.length - 1;
+        while (j > i) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
 
-     }
+        for (int k = lastPos + 1; k < nums.length; ++k) {
+            if (nums[k] > nums[lastPos]) {
+                swap(nums, k, lastPos);
+                break;
+            }
+        }
+    }
 }
