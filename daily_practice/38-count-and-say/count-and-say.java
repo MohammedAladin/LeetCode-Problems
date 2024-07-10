be8f0a83a3120c909a78cc7d41227edf28d2ed5a
MohammedAladin <69739865+MohammedAladin@users.noreply.github.com>
2024-07-10 16:41:57 +0300

    Time: 6 ms (49.85%) | Memory: 40.9 MB (90.31%) - LeetSync

--- daily_practice/38-count-and-say/count-and-say.java
+++ daily_practice/38-count-and-say/count-and-say.java
@@ -1,32 +1,25 @@
 class Solution {
-      String ans="";
-    private void solve(int itr, int n, String seq){
-
-        if(itr == n) {
-            ans = seq;
-            return;
-        }
-        String str="";
-        int cnt = 1;
-        char c = seq.charAt(0);
-        for(int i = 1; i<seq.length(); ++i){
-            if(c != seq.charAt(i)){
-                str+=String.valueOf(cnt)+c;
-                c = seq.charAt(i);
-                cnt = 1;
+   public String countAndSay(int n) {
+        String ans = "1";  // Initial sequence
+        
+        for (int itr = 1; itr < n; itr++) {
+            StringBuilder str = new StringBuilder();
+            int cnt = 1;
+            char c = ans.charAt(0);
+            
+            for (int i = 1; i < ans.length(); i++) {
+                if (c != ans.charAt(i)) {
+                    str.append(cnt).append(c);
+                    c = ans.charAt(i);
+                    cnt = 1;
+                } else {
+                    cnt++;
+                }
             }
-            else{
-                cnt++;
-            }
+            str.append(cnt).append(c);
+            ans = str.toString();
         }
-        str+=String.valueOf(cnt)+c;
-        solve(itr+1, n, str);
-
-
-    }
-
-    public String countAndSay(int n) {
-        solve(1,n,"1");
+        
         return ans;
     }
 }
